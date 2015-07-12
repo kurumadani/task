ce470491e12d7de97ce72020c969fa2582d71daa
kurumadani <kaka_yoshi01@yahoo.co.jp>
2015-07-13 04:37:00 +0900

    ５－３動的にレコードを選択しよう「最初にhtmlをかいてみよう」

---
+++ Chapter5/select_goods.php
@@ -0,0 +1,17 @@
+<!-- 5-3 動的にレコードを選択してみよう -->
+<!-- 最初にhtmlをかいてみよう -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>売上管理システム</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1>商品マスタの選択</h1>
+<form action="" method="post">
+<label>GoodID<input type= "text" name="GoodID" size="20" required></label>
+<input type="submit" name="select" value="　表示　">
+</form>
+</body>
+</html>
